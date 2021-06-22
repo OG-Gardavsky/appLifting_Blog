a84cb49e6d819a9737ecd2b362e56e1ce46ff71c
OG-Gardavsky <ogardavsky@gmail.com>
2021-06-22 17:39:35 +0200

    Basic UI setup

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+
+
+        <ul class="nav">
+
+            <span id="rightLinks" class="d-flex flex-row">
+                <img alt="Vue logo" src="../assets/cat.png" width="60px">
+
+                <span> <router-link to="/">Recent Articles</router-link> </span>
+
+                <span> <router-link to="/about">About</router-link> </span>
+
+            </span>
+
+            <li class="nav-item d-flex flex-row">
+                <a class="nav-link" href="#">Log in</a>
+                <i class="fas fa-arrow-right"></i>
+            </li>
+
+        </ul>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+     name: "Navbar"
+}
+</script>
+
+<style scoped lang="scss">
+    div {
+        background-color: #f3f2f2;
+        padding-top: 10px;
+
+        .nav {
+            max-width: 70%;
+            margin: 0 auto;
+            justify-content: space-between;
+
+            #rightLinks {
+
+
+                //&.router-link-exact-active
+
+                span {
+                    margin: 10px;
+                }
+
+
+            }
+
+
+
+            .fa-arrow-right {
+                margin: 10px 0;
+            }
+
+        }
+    }
+
+
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <Navbar />
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+
+<script>
+
+import Navbar from "@/components/Navbar";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: {
+        Navbar
+
+    }
 }
+
 </script>
