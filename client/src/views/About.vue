e1b506384806acea5867cdba6ab3bbf83abb1296
OG-Gardavsky <ogardavsky@gmail.com>
2021-06-27 22:29:44 +0200

    About page filled with content

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -3,7 +3,15 @@
 
         <Navbar />
 
-        <h1>About Page</h1>
+        <div class="main">
+            <h1>About Page</h1>
+            <p>This is just about blank about page, nothing really special here. Besides Lorem Ipsum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at dictum sapien, in imperdiet nisl. Nam tellus libero, pulvinar eget sapien in, vehicula scelerisque ligula. Vivamus semper bibendum mi sit amet rhoncus. Sed pretium ullamcorper finibus. Aliquam cursus ornare nisi sit amet mollis. In vestibulum orci a dolor bibendum pellentesque. Curabitur vitae sem quis est fermentum vulputate nec id justo. Suspendisse nisi diam, vulputate et cursus et, fermentum elementum ex. In et nisl quis erat tristique tempus. Donec sed urna ac ipsum aliquet mattis. Nam elementum neque ac fermentum efficitur. Nulla ac turpis ac quam hendrerit pellentesque vel at neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+
+        </div>
+
+
 
     </div>
 </template>
