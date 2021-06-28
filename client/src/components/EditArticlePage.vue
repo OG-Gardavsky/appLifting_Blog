b44f79ef7caa3b5a720cfc1146f12a66c743b057
OG-Gardavsky <ogardavsky@gmail.com>
2021-06-28 21:56:50 +0200

    Splited create article into component

---
+++ client/src/components/EditArticlePage.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <form>
+            <div class="form-group">
+                <label class="d-flex">Article Title</label>
+                <input type="text" class="form-control" v-model="title" placeholder="My first article" />
+            </div>
+
+            <div class="form-group">
+                <label class="d-flex">Featured image (not implemented)</label>
+                <button class="btn btn-secondary d-flex">Upload an image</button>
+            </div>
+
+            <div class="form-group">
+                <label class="d-flex">Perex</label>
+                <textarea class="form-control" id="perex"  v-model="perex" placeholder="Leading Paragraph" rows="4"/>
+            </div>
+
+            <div class="form-group">
+                <label class="d-flex">Content</label>
+                <textarea class="form-control" id="content"  v-model="content" placeholder="Supports markdown. Yay!" rows="18"/>
+            </div>
+        </form>
+
+
+        <!-- markdown preview -->
+        <div style="margin-top: 30px" v-if="![title, content].includes(null)">
+            <h2 class="d-flex">Preview</h2>
+            <hr />
+            <h1 class="d-flex">{{title}}</h1>
+            <vue-markdown :source="content" />
+        </div>
+
+    </div>
+</template>
+
+<script>
+import VueMarkdown from 'vue-markdown';
+
+export default {
+    name: "EditArticlePage",
+    components: {
+        VueMarkdown
+    },
+    props: {
+        title: String,
+        perex: String,
+        content: String
+    }
+    // ,
+    // methods: {
+    //     createArticle() {
+    //         if ([this.title, this.perex, this.content].includes(null)) {
+    //             return this.setGenericError(this.genericError, true, 'Please fill "Title", "Perex" and "Content"');
+    //         }
+    //
+    //
+    //         const body = {
+    //             title: this.title,
+    //             perex: this.perex,
+    //             content: this.content
+    //         };
+    //
+    //         this.$emit('on-login', body);
+    //
+    //     }
+    // }
+}
+</script>
+
+<style scoped>
+
+</style>
--- client/src/views/CreateArticle.vue
+++ client/src/views/CreateArticle.vue
@@ -10,38 +10,7 @@
 
             <generic-error :display="genericError.display" :text="genericError.text" />
 
-            <form>
-                <div class="form-group">
-                    <label class="d-flex">Article Title</label>
-                    <input type="text" class="form-control" v-model="title" placeholder="My first article" />
-                </div>
-
-                <div class="form-group">
-                    <label class="d-flex">Featured image (not implemented)</label>
-                    <button class="btn btn-secondary d-flex">Upload an image</button>
-                </div>
-
-                <div class="form-group">
-                    <label class="d-flex">Perex</label>
-                    <textarea class="form-control" id="perex"  v-model="perex" placeholder="Leading Paragraph" rows="4"/>
-                </div>
-
-                <div class="form-group">
-                    <label class="d-flex">Content</label>
-                    <textarea class="form-control" id="content"  v-model="content" placeholder="Supports markdown. Yay!" rows="18"/>
-                </div>
-            </form>
-
-
-            <!-- markdown preview -->
-            <div style="margin-top: 30px" v-if="![title, content].includes(null)">
-                <h2 class="d-flex">Preview</h2>
-                <hr />
-                <h1 class="d-flex">{{title}}</h1>
-                <vue-markdown :source="content" />
-            </div>
-
-
+            <edit-article-page :content="content" :perex="perex" :title="title"/>
 
         </div>
     </div>
@@ -49,16 +18,16 @@
 
 <script>
 import Navbar from "@/components/Navbar";
-import VueMarkdown from 'vue-markdown';
 import GenericError from "@/components/GenericError";
 import router from "@/router";
+import EditArticlePage from "@/components/EditArticlePage";
 
 export default {
     name: "CreateArticle",
         components: {
+            EditArticlePage,
             GenericError,
-            Navbar,
-            VueMarkdown
+            Navbar
         },
     data() {
         return {
