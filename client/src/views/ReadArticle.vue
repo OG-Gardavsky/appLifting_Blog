03d644757aa5d1a2647ba25fe6d2effffa3d2b8c
OG-Gardavsky <ogardavsky@gmail.com>
2021-06-28 08:39:01 +0200

    Adding comment work on FE

--- client/src/views/ReadArticle.vue
+++ client/src/views/ReadArticle.vue
@@ -4,20 +4,45 @@
 
         <div class="main">
 
+            <!-- main article -->
             <h1>{{articleDetails.title}}</h1>
             <img class="img-fluid" src="../assets/general_cat_image.jpg">
             <vue-markdown :source="articleDetails.content" />
             <hr/>
 
+            <!-- comment section -->
             <h2>Comments (4)</h2>
 
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" v-model="newComment" placeholder="Join the discussion" >
-                <div class="input-group-append" v-if="newComment !== ''">
-                    <button class="btn btn-outline-secondary" type="button">send comment</button>
+            <!-- adding comment -->
+            <div>
+
+                <div class="form-group">
+                    <label class="d-flex" v-if=" newComment.content !== '' ">Your comment</label>
+                    <input type="text" class="form-control" v-model="newComment.content" placeholder="Join the discussion" >
                 </div>
+
+                <div v-if=" newComment.content !== '' ">
+
+                    <div class="form-group">
+                        <label class="d-flex">Your name</label>
+                        <input type="text" class="form-control" v-model="newComment.author" placeholder="Your name" >
+                    </div>
+
+                    <button class="btn btn-secondary" type="button" @click="createComment()">send comment</button>
+
+                </div>
+
             </div>
 
+            <generic-error :display="genericError.display" :text="genericError.text" />
+
+            <!-- displaying comments -->
+            <div
+                :key="comment._id" v-for="comment in articleComments"
+            >
+                <p> {{comment.content}}  {{comment.author}} </p>
+
+            </div>
 
 
         </div>
@@ -28,16 +53,23 @@
 <script>
 import Navbar from "@/components/Navbar";
 import VueMarkdown from 'vue-markdown';
+import GenericError from "@/components/GenericError";
 export default {
     name: "ReadArticle",
     components: {
+        GenericError,
         Navbar,
         VueMarkdown
     },
     data() {
         return {
+            articleId: null,
             articleDetails: null,
-            newComment: ''
+            articleComments: null,
+            newComment: {
+                content: '',
+                author: null
+            }
         }
     },
     methods: {
@@ -47,17 +79,43 @@ export default {
             if (res.status === 200) {
                 this.articleDetails = await res.json();
             }
+        },
+        async createComment() {
+
+            if ([this.newComment.content, this.newComment.author].includes(null)) {
+                return this.setGenericError(this.genericError, true, 'Please fill both your name and text of your comment');
+            }
+
+            this.newComment.articleId = this.articleId;
+            const res = await this.sendHttpRequest(`comments`, 'POST', false, this.newComment);
+
+            if (res.status === 201) {
+                await this.getArticleComments(this.articleId);
+            }
+        },
+        async getArticleComments(articleId) {
+            const res = await this.sendHttpRequest(`/comments/articleId:${articleId}`, 'GET', false);
+
+            if (res.status === 200) {
+                this.articleComments = await res.json();
+            }
         }
     },
     created() {
+        this.articleId = this.$route.query.id
 
-        this.getArticleDetails(this.$route.query.id);
+        this.getArticleDetails(this.articleId);
+        this.getArticleComments(this.articleId);
 
     }
 
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
+    input {
+        margin: 10px 0;
+    }
 
 </style>
